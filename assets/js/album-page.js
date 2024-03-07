a8131f87fb2a673df35f2952fac33097b538cc53
RiccardoAD <rick_95@libero.it>
2024-03-07 21:43:44 +0100

    subtleprove

--- assets/js/album-page.js
+++ assets/js/album-page.js
@@ -6,11 +6,11 @@ const headers = {
   },
 };
 
-// const url = "https://deezerdevs-deezer.p.rapidapi.com/album/";
-// const id = "248216622";
+const url = "https://deezerdevs-deezer.p.rapidapi.com/album/";
+const id = "248216622";
 
-const url = window.location.pathname;
-const id= url.split(`/`).pop()
+// const url = window.location.pathname;
+// const id= url.split(`/`).pop()
 
 
 document.addEventListener("DOMContentLoaded", function () {
